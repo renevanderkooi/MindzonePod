96372cd2f93821e5910911a4565e80a3a416b2e0
Rene van der Kooi <rkooi@mind-zone.net>
2021-07-17 12:06:33 +0700

    Add test case

---
+++ MindzonePod/MindzonePod.swift
@@ -0,0 +1,17 @@
+//
+//  MindzonePod.swift
+//  MindzonePod
+//
+//  Created by Rene van der Kooi on 17/7/2564 BE.
+//
+
+import Foundation
+
+public final class MindzonePod {
+
+    let name = "MindzonePod"
+    
+    public func doLog(a: String) -> Bool {
+        return true
+    }
+}
