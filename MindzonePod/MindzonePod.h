4492dfd416e0d0f7a7932cf21efeaf6f2d67d3e2
Rene van der Kooi <rkooi@mind-zone.net>
2021-07-17 12:00:06 +0700

    Project Setup

---
+++ MindzonePod/MindzonePod.h
@@ -0,0 +1,18 @@
+//
+//  MindzonePod.h
+//  MindzonePod
+//
+//  Created by Rene van der Kooi on 17/7/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MindzonePod.
+FOUNDATION_EXPORT double MindzonePodVersionNumber;
+
+//! Project version string for MindzonePod.
+FOUNDATION_EXPORT const unsigned char MindzonePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MindzonePod/PublicHeader.h>
+
+
---
+++ MindzonePodExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindzonePodExamples
+//
+//  Created by Rene van der Kooi on 17/7/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
